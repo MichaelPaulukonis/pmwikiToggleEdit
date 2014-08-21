7205c1fc4c058a30277efc81ccfd92b82e7cd53f
MichaelPaulukonis <xraysmalevich@gmail.com>
2014-08-21 13:44:32 -0400

    new makeDiv func for pmAddToggle

--- pub/pm-toggle-edit.js
+++ pub/pm-toggle-edit.js
@@ -3,14 +3,41 @@
 // for integration with http://www.pmwiki.org/wiki/Cookbook/Toggle
 // and http://www.pmwiki.org/wiki/Cookbook/GuiEdit
 
-function pmAddToggle() {
+function pmAddToggle(style, cls) {
     // basic toggle code for one-box on/off
-    // TODO: class for better CSS handling
     // use the (:div<n>:)...(:div<n>end:) markup to allow for toggling nested divs
-    var uniqueNbr = (new Date).getTime();
-    var idName = 'tog'+uniqueNbr;
-    var open = '(:toggle id="'+idName+'" show="Show message" init=hide button=1:)\n';
-    open += '(:div'+uniqueNbr+' id="'+idName+'" style=\"border:1px solid #999; padding:5px;\":)\n';
-    var close = '\n(:div'+uniqueNbr+'end:)';
+
+	style = style || 'border:1px solid #999; padding:5px;';
+	cls = cls || '';
+	var div = makeDiv(style, cls, '', 'tog');
+	var id = div.id;
+		
+    var open = '(:toggle ' + id + ' show="Show message" init=hide button=1:)\n';
+	
+	open += div.open + '\n';
+	var close = '\n' + div.close;
+	
     insMarkup(open,close,'toggle this text');
+}
+
+// create pmWiki div markup with id (optional prefix), class and style attributes
+function makeDiv(style, cls, id, prefix) {
+
+	style = (style ? 'style=\"' + style + '\"' : '' );
+	cls = (cls ? 'class=\"' + cls + '\"' : '');
+	prefix = prefix || '';
+	
+	var uniqueNbr = (new Date).getTime();
+    id = id || uniqueNbr;
+    var idFull = 'id=\"' + prefix + id + '\"';
+		
+	var builder = ['(:div' + uniqueNbr, idFull, cls, style, ':)'];	
+
+	var open = builder.join(' ');	
+    var close = '(:div'+uniqueNbr+'end:)';
+
+	return { id: idFull,
+			 open: open,
+			 close: close
+		    };
 }
